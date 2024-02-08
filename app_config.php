fe2def723eb99a34a54a351d5da45ffb25d8a726
Budhaditya Bose <48716228+Vyiel@users.noreply.github.com>
2024-02-08 06:03:57 -0800

    Delete app_config.php

--- app_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-	// CHANGE THESE ACCORDING TO YOUR REQUIRMENTS //
-	$vmrun_location = '"F:\\Program Files (x86)\\VMware\\VMware Workstation\\vmrun.exe"';
-	$host_location = 'professorveil.ddns.net'
-
-?>
